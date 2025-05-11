4befea1b9ea30ac7d3a57ea4f7ad14e79150261a
zhengjiaming50 <zhengzhetian50@gmail.com>
2025-05-12 00:44:28 +0800

    Revert "feat: 不行"
    This reverts commit 20eddf8d1fbae18ca7acaac1306ffb5558de017d.

--- src/stores/slices/environment_model_store.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from "jotai";
-
-interface EnvironmentModelState {
-  environmentModelUrl: string | null;
-}
-
-export const environmentModelStore = atom<EnvironmentModelState>({
-  environmentModelUrl: null,
-});
